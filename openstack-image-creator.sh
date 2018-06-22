1190a22113add6a3052cadd2ed04458cbd45970a
Alexey Bychko <abychko@gmail.com>
2018-06-22 14:25:40 +0700

    dixed debian build

--- openstack-image-creator.sh
+++ openstack-image-creator.sh
@@ -207,7 +207,9 @@ mount -o loop /dev/mapper/${LOOP_DEVICE} ${MOUNTDIR}
 export UUID=$(blkid -o value -s UUID /dev/mapper/${LOOP_DEVICE})
 mkdir -p ${MOUNTDIR}/{proc,sys,dev,etc}
 ##
-#
+if [ ${DISTRO} != centos ]; then
+  installBaseSystem
+fi
 # mount /proc, /dev, /sys
 mount -t proc /proc ${MOUNTDIR}/proc
 mount --rbind /sys ${MOUNTDIR}/sys
@@ -217,7 +219,9 @@ mount --make-rslave ${MOUNTDIR}/dev
 #
 cat /etc/resolv.conf > ${MOUNTDIR}/etc/resolv.conf
 #
-installBaseSystem
+if [ ${DISTRO} = centos ]; then
+  installBaseSystem
+fi
 #
 chroot ${MOUNTDIR} rm -f /etc/mtab
 chroot ${MOUNTDIR} ln -s /proc/mounts /etc/mtab
